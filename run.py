cf4e1b3e6752ecf8f969e33b56267c4eabc54206
skpprafful <skpprafful@gmail.com>
2022-08-30 23:29:59 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 #!flask/bin/python
-from flask import Flask, render_template
-app = Flask(__name__, static_url_path='/static')
+from flask import Flask
+app = Flask(__name__)
 
-@app.route("/")
+
 def run():
     try:
-        app.run(host='127.0.0.1', port=5000, threaded=True)
+        app.run(host='0.0.0.0', port=5000, threaded=True)
     except OSError:
         print('Website is already running!')
         print('Browse to http://localhost:5000')
-    print(app.route)
-    return render_template('index.html')
